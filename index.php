7a4dd1048e68ff45d799d0fda05ec69a719ecb39
sent1nel101 <37023270+sent1nel101@users.noreply.github.com>
2021-11-18 12:58:05 -0500

    project complete

--- index.php
+++ index.php
@@ -30,7 +30,6 @@
 		}
 
 }
-
 ?>
 
 
@@ -45,17 +44,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   </head>
   <body>
+    <div id="appContainer">
 
-
-	  <div id="appContainer">
-
-		  <h1>What's The Weather?</h1>
+      <h1>What's The Weather?</h1>
 			<form>
-			  <fieldset id="fieldset" class="form-group">
+        <fieldset id="fieldset" class="form-group">
 				<label for="city"><h4>Enter the name of a city</h4></label>
 				<input type="text" class="form-control" name="city" id="city" placeholder="e.g. London, Tokyo">
-			  </fieldset>
-			  <button type="submit" class="btn btn-primary">Submit</button>
+        </fieldset>
+        <button type="submit" class="btn btn-primary">Submit</button>
 			</form>
 			<br>
 
@@ -69,7 +66,7 @@
 			?>
 			</div>
 			
-	  </div>
+    </div>
 
     <script type="text/javascript" src="jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
